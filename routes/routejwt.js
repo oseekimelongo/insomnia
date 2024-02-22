2a8dc78a60bd43b7d912a40379793017930608ab
osee kimelongo <oseekimelongo@gmail.com>
2024-02-22 17:14:01 +0200

    modification const sur jwt

---
+++ routes/routejwt.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('../controllers/jwtcontroller');
+const {upload} = require('./storage');
+
+
+
+
+router.post('/loginjwt',jwt.loginjwtUserPost);
+
+
